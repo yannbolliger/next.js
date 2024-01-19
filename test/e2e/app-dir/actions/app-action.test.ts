56cfdd6d02ca3d213550ea4f3a50ad6a20133cc2
hrmny <8845940+ForsakenHarmony@users.noreply.github.com>
2024-01-19 21:33:33 +0100

    more fixes

--- test/e2e/app-dir/actions/app-action.test.ts
+++ test/e2e/app-dir/actions/app-action.test.ts
@@ -1,7 +1,7 @@
 /* eslint-disable jest/no-standalone-expect */
 import { createNextDescribe } from 'e2e-utils'
 import { check, waitFor } from 'next-test-utils'
-import { Request, Response } from 'playwright-chromium'
+import type { Request, Response } from 'playwright'
 import fs from 'fs-extra'
 import { join } from 'path'
 
